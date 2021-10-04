5fcf927b8eb18d876191ad83a2e2fd738c14a16b
JoeyDela <duffy.west87@gmail.com>
2021-10-03 20:19:48 -0500

    small start

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = ('inquirer');
+
+
+const html = require('');
+
+var fs = require("fs");{
+    fs.writeToFile(filename, data, error => {
+        error ? console.log(error) : console.log('Page generated Successfully!');
+    });
+};
+
