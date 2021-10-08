0b7950563851aa372fd889f874b4bc2203154c7c
JoeyDela <duffy.west87@gmail.com>
2021-10-07 20:02:06 -0500

    final push

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const inquirer = ('inquirer');
+const inquirer = require("inquirer");
+ const fs = require("fs");
+ const path = require('path');
 
 
-const html = require('');
-
-var fs = require("fs");{
-    fs.writeToFile(filename, data, error => {
-        error ? console.log(error) : console.log('Page generated Successfully!');
-    });
-};
-
+ const employee = [];
+ function init() {
+     startHTML();
+     addToRoster();
+    
+     function addToRoster() {
+         inquirer
+             .promt([
+             {
+                 type: "input",
+                 name: "managerName",
+                 message: "What is your Manager's name?"
+             },
+             {
+                 type: "input",
+                 name: "managerId",
+                 message: "What is your Id?"
+             },
+             {
+                 type: "input",
+                 name: "managerEmail",
+                 message: "What is your Email?"
+             },
+             {
+                 type: "input",
+                 name: "officeNumber",
+                 message: "What is your Office Number?"
+             },
+         ]).then(response => {
+             const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.officeNmber);
+             teamMember.push(manager);
+             addingNewMember();
+         })
+     };
+     const draft = generate(team);
+     fs.writeFile(outputPath, draft, err => {
+         err ? console.log(err) : console.log('== Team profile successfully created!')
+     })
+ }
+ init();
